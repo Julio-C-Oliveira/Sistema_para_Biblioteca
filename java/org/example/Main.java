48a40d996480cb8163818a6f121f05190fa95c6c
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-03-15 11:25:48 -0300

    Corrigindo Json e path O merge anterior gerou um erro no json de notificações, erro corrigido. O path da main foi alterado e parou de funcionar, o ~ que é o caminho relativo não funciona no meu PC quando eu insiro isso na String de path, tenho que colocar o caminho absoluto mesmo.

--- java/org/example/Main.java
+++ java/org/example/Main.java
@@ -1,63 +1,16 @@
 package org.example;
 
-import org.example.database.User.*;
 import org.example.display.UserDisplay;
-import org.example.login.Login;
-import org.example.notifications.Notifications;
+import org.example.notifications.NotificationsDataManager;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
 
 public class Main {
-    public final static String basePath = "~/IdeaProjects/Sistema_para_Biblioteca_Gradle/src/";
+    public final static String basePath = "/home/juliocoliveira/IdeaProjects/Sistema_para_Biblioteca_Gradle/src/";
 
     public static void main(String[] args) throws IOException {
-        // CORRIGIR BUD DO ID
+        UserDisplay display = new UserDisplay();
 
-        CreateUserTable.createTable();
-        // Criando alguns participantes
-        InsertUser.create("Savio", 2, "bacuri");
-        InsertUser.create("Julio", 1, "goiaba");
-        InsertUser.create("Luiz", 3, "cupuacu");
-        InsertUser.create("Reginaldo", 3, "maca");
-        InsertUser.create("Yuri", 3, "pera");
-
-        // Testando atualização de participante
-        System.out.println("Update:");
-        UpdateUser.updateUser("Yuri", "buriti");
-
-        // Lendo participantes
-
-         Map<String, String> yuri = GetUser.getUser("Yuri");
-         System.out.println(yuri.get("name"));
-
-         Map<String, String> alguem = GetUser.getUser(3);
-         System.out.println(alguem.get("role"));
-
-        // Testando deleção de participante
-         //DeleteUser.delete(4);
-
-        // Testando o "login"
-        Login login = new Login();
-        Map<String, String> user = Map.of(
-                "name", "Luiz",
-                "role", "1",
-                "password", "buriti"
-        );
-        if(login.validateLogin(user)){
-            System.out.println("Login successful");
-        } else {
-            System.out.println("Login failed");
-        }
-
-        ArrayList<Map<String, String>> users = GetAllUsers.getAll();
-        for (Map<String, String> i : users) {
-            System.out.println(i.get("name"));
-        }
-
-//        UserDisplay display = new UserDisplay();
-
-//        display.runInterface();
+        display.runInterface();
     }
 }
